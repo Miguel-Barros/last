1cacadbaba6c0b8f5a67e83f680e7d9a5d49a223
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-11-18 14:17:38 -0300

    Restore

--- src/components/scrollBar.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles/ScrollBar.module.css";
-
-export default function ScrollBar() {
-  return (
-    <span className={styles.scroll}>
-      <span className={styles.dumb}></span>
-    </span>
-  )
-}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,5 @@
-import styles from './styles/Index.module.css'
-import Nav from '../components/nav'
-import AutoWrite from '../components/autoWrite'
-import { Icon } from '@iconify/react';
+import styles from './styles/Index.module.scss'
 import Head from 'next/head'
-import ScrollBar from '../components/scrollBar';
-import SideBar from '../components/sideBar';
 
 export default function Home() {
   return (
@@ -12,26 +7,12 @@ export default function Home() {
       <Head>
         <title>Last</title>
       </Head>
-      <Nav/>
       <main className={styles.main}>
-      <span className={`${styles.glass} ${styles.secundary}`}/>
-      <span className={`${styles.glass}`}/>
-        <div className={styles.box}>
-          <AutoWrite/>
-          <p>Seja bem vindo ao meu portifolio pessoal. Sinta-se livre para observar e avaliar alguns dos meus trabalhos pessoais e corporativos.</p>
-        </div>
-        <span className={styles.illustration}>
-          <img src="./assets/ilus.svg" alt="illustration" />
-        </span>
       </main>
       <footer className={styles.footer}>
-        <span>
-          <p>Conhecer mais</p>
-          <Icon icon={'mdi:chevron-down'} className={styles.arrow}/>
-        </span>
       </footer>
     </div>
   )
 }
 
-{/*<p className={styles.copy}>Miguel Barros © 2022</p>*/}
+{/*<p className={styles.copy}>Miguel Barros © 2022</p>*/ }
