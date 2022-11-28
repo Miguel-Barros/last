90a493e1919a58658d77ae1521e5c9deac49812e
Ana Beatriz Costa de Sousa <anacostasousa08@gmail.com>
2022-11-28 11:55:31 -0300

    Create about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,6 +2,7 @@ import styles from './styles/About.module.scss'
 import Head from 'next/head'
 import Nav from '../components/nav'
 import ContactsBar from '../components/contacts'
+import Scroll from '../components/scroll'
 
 export default function About() {
 
@@ -15,8 +16,17 @@ export default function About() {
             <main className={styles.main}>
                 <div className={styles.left}>
                     <img src="https://avatars.githubusercontent.com/u/79284434?v=4" alt="Profile-Github" />
+                    <p>Programador</p>
+                    <p>Full Stack & UX Design</p>
+                </div>
+                <div className={styles.right}>
+                    <p>Eu sou <span> Miguel de Barros </span><br />
+                        Minha conexão com a tecnologia
+                        foi paixão a primeira vista, esse universo de possibilidades infinitas é muito fascinante, mostrou-me um caminho e tornou quem sou hoje.
+                    </p>
                 </div>
             </main>
+            <Scroll />
         </div>
     )
 }
